9b6a91e9a2dfb0cee19d354c6df70ce98ce517f0
Guillaume Foisy <foisyguillaume@gmail.com>
2026-04-14 22:19:39 -0400

    Update tls_wrapper_v3.h

--- src/wrappers/c/tls_wrapper_v3.h
+++ src/wrappers/c/tls_wrapper_v3.h
@@ -7,9 +7,18 @@
 extern "C" {
 #endif
 
-
-
-
+// ---------------------------------------------------------------------------
+// EXPORT MACRO (Windows DLL / other platforms)
+// ---------------------------------------------------------------------------
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef TLSV2_BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+  #else
+    #define DLL_EXPORT __declspec(dllimport)
+  #endif
+#else
+  #define DLL_EXPORT
+#endif
 
 // ---------------------------------------------------------------------------
 // SERVER CONFIG STRUCT
@@ -30,33 +39,27 @@ typedef struct {
 
 } tlsv2_server_config_t;
 
-
-void tlsv2_set_callbacks(
-    tlsv2_on_client_connected,
-    tlsv2_on_client_disconnected,
-    tlsv2_on_json_received);
-    
-    
 // ---------------------------------------------------------------------------
 // SERVER API
 // ---------------------------------------------------------------------------
 
-int tlsv2_server_run(const tlsv2_server_config_t *cfg);
-int tlsv2_send_json(int client_id, const char *json, size_t len);
+DLL_EXPORT void tlsv2_set_callbacks(
+    tlsv2_on_client_connected,
+    tlsv2_on_client_disconnected,
+    tlsv2_on_json_received);
+
+DLL_EXPORT int tlsv2_server_run(const tlsv2_server_config_t *cfg);
+DLL_EXPORT int tlsv2_send_json(int client_id, const char *json, size_t len);
 
 // ---------------------------------------------------------------------------
 // CLIENT API (blocking, PB/FB friendly)
 // ---------------------------------------------------------------------------
 
-int tlsv2_client_init(void);
-
-int tlsv2_client_connect(const char *host, int port);
-
-int tlsv2_client_send_json(int sock, const char *json, size_t len);
-
-int tlsv2_client_recv_json(int sock, char *buf, size_t maxlen);
-
-void tlsv2_client_close_fd(int sock);
+DLL_EXPORT int tlsv2_client_init(void);
+DLL_EXPORT int tlsv2_client_connect(const char *host, int port);
+DLL_EXPORT int tlsv2_client_send_json(int sock, const char *json, size_t len);
+DLL_EXPORT int tlsv2_client_recv_json(int sock, char *buf, size_t maxlen);
+DLL_EXPORT void tlsv2_client_close_fd(int sock);
 
 #ifdef __cplusplus
 }
